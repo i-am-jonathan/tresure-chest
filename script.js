7c3e8699aa16000c7cb1d68bdf0bf037114b825c
i-am-jonathan <43018334+i-am-jonathan@users.noreply.github.com>
2018-12-19 14:58:30 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#button1").click(function(){
+    $("#double").show();
+});
+ $("#double").hide();
+  $("#coins").hide();
+  $("#coin").hide();
+  $("#double").click(function(){
+  $("#coins").show();
+   $("#coin").show();
+});
+ $("#coin pile").hide();
+  $("coin").hover(function(){
+       $("coin pile").text("");
+      
+});
